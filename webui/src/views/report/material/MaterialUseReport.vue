7f175725b972f48e859a1b2a266046ce3dcc542a
YRK <1973205238@qq.com>
2021-05-19 19:00:19 +0800

    Delete logs directory
    MaterialUseReport.vue功能完成

--- webui/src/views/report/material/MaterialUseReport.vue
+++ webui/src/views/report/material/MaterialUseReport.vue
@@ -10,17 +10,25 @@
           :rules="formRules"
           @submit="handlerSubmit"
         >
-          <vxe-form-item span="5" title="开始时间" field="startTime">
-            <vxe-input v-model="form.startTime" placeholder="日期选择" type="date"></vxe-input>
+          <vxe-form-item span="5" title="开始时间" field="startTime" :item-render="{}">
+            <template #default>
+              <vxe-input v-model="form.startTime" placeholder="日期选择" type="date"/>
+            </template>
           </vxe-form-item>
-          <vxe-form-item span="5" title="结束时间" field="endTime">
-            <vxe-input v-model="form.endTime" placeholder="日期选择" type="date"></vxe-input>
+          <vxe-form-item span="5" title="结束时间" field="endTime" :item-render="{}">
+            <template #default>
+              <vxe-input v-model="form.endTime" placeholder="日期选择" type="date"></vxe-input>
+            </template>
           </vxe-form-item>
-          <vxe-form-item span="5" title="产品ID" field="productID">
-            <vxe-input v-model="form.productID" placeholder="请输入产品ID" type="text"></vxe-input>
+          <vxe-form-item span="5" title="产品ID" field="productID" :item-render="{}">
+            <template #default>
+              <vxe-input v-model="form.productID" placeholder="请输入产品ID" type="text"></vxe-input>
+            </template>
           </vxe-form-item>
-          <vxe-form-item span="5" title="物料ID" field="materialID">
-            <vxe-input v-model="form.materialID" placeholder="请输入物料ID" type="text"></vxe-input>
+          <vxe-form-item span="5" title="物料ID" field="materialID" :item-render="{}">
+            <template #default>
+              <vxe-input v-model="form.materialID" placeholder="请输入物料ID" type="text"></vxe-input>
+            </template>
           </vxe-form-item>
           <vxe-form-item>
             <template #default>
@@ -33,25 +41,27 @@
     </a-row>
     <a-spin :spinning="loading" tip="查询中...">
       <data-table
-        title='物料使用记录报表'
-        :height='height'
-        :columns='columns'
-        :datasource='datasource'
+        title="物料使用记录报表"
+        :height="height"
+        :columns="columns"
+        :datasource="datasource"
       />
     </a-spin>
   </div>
 </template>
 
 <script>
-import BarChart from "@comp/chart/BarChart";
-import QuerySelect from "@comp/QuerySelect";
-import LineChart from "@comp/chart/LineChart";
-import {getCumulative, getInstantaneous} from "@api/energyApi"
-import {transferStringToArray} from "@/utils/util";
+import BarChart from '@comp/chart/BarChart'
+import QuerySelect from '@comp/QuerySelect'
+import LineChart from '@comp/chart/LineChart'
+import { getCumulative, getInstantaneous } from '@api/energyApi'
+import { transferStringToArray } from '@/utils/util'
+import { postAction } from '@api/manage'
 import DataTable from '@comp/DataTable'
+import Vue from 'vue'
 
 export default {
-  name: "MaterialUseReport",
+  name: 'MaterialUseReport',
   components: {
     DataTable,
     LineChart,
@@ -71,7 +81,14 @@ export default {
         productID: '',
         materialID: ''
       },
-      formRules: {},
+      formRules: {
+        'startTime': [{
+          required: true
+        }],
+        'endTime': [{
+          required: true
+        }]
+      },
       switchWaterUseUnit: '%Y-%m-%d',
       switchCOPUnit: '%Y-%m-%d',
       switchElcUnit: '%Y-%m-%d',
@@ -113,14 +130,14 @@ export default {
       switchArea: 'T',
       areaTagList: 'PEMS_LCHW_ColdCapacityT.VAL_Actl,PEMS_MCHW_ColdCapacityT.VAL_Actl,PEMS_RCHW_ColdCapacityT.VAL_Actl',
       columns: [
-        {title: '物料ID', field: 'ID', align: 'center', sortable: true},
-        {title: '物料名称', field: 'Name', align: 'center'},
-        {title: '供应商', field: 'suplyer', align: 'center'},
-        {title: '生产日期', field: 'productionDate', align: 'center'},
-        {title: '入厂日期', field: 'inWMSDate', align: 'center'},
-        {title: '接收人', field: 'reciver', align: 'center'},
-        {title: '绑定产品ID', field: 'productionID', align: 'center', sortable: true},
-        {title: '使用时间', field: 'useTime', align: 'center'}
+        { title: '物料ID', field: 'ID', align: 'center', sortable: true },
+        { title: '物料名称', field: 'Name', align: 'center' },
+        { title: '供应商', field: 'suplyer', align: 'center' },
+        { title: '生产日期', field: 'productionDate', align: 'center' },
+        { title: '入厂日期', field: 'inWMSDate', align: 'center' },
+        { title: '接收人', field: 'reciver', align: 'center' },
+        { title: '绑定产品ID', field: 'productionID', align: 'center', sortable: true },
+        { title: '使用时间', field: 'useTime', align: 'center' }
       ],
       datasource: []
     }
@@ -153,16 +170,16 @@ export default {
       if (this.form.startTime && this.form.endTime) {
         switch (this.switchArea) {
           case 'T':
-            this.areaTagList = 'PEMS_LCHW_ColdCapacityT.VAL_Actl,PEMS_MCHW_ColdCapacityT.VAL_Actl,PEMS_RCHW_ColdCapacityT.VAL_Actl';
-            break;
+            this.areaTagList = 'PEMS_LCHW_ColdCapacityT.VAL_Actl,PEMS_MCHW_ColdCapacityT.VAL_Actl,PEMS_RCHW_ColdCapacityT.VAL_Actl'
+            break
           case '1':
-            this.areaTagList = 'PEMS_LCHW_ColdCapacity1T.VAL_Actl,PEMS_MCHW_ColdCapacity1T.VAL_Actl,PEMS_RCHW_ColdCapacity1T.VAL_Actl';
-            break;
+            this.areaTagList = 'PEMS_LCHW_ColdCapacity1T.VAL_Actl,PEMS_MCHW_ColdCapacity1T.VAL_Actl,PEMS_RCHW_ColdCapacity1T.VAL_Actl'
+            break
           case '2':
-            this.areaTagList = 'PEMS_LCHW_ColdCapacity2T.VAL_Actl,PEMS_MCHW_ColdCapacity2T.VAL_Actl,PEMS_RCHW_ColdCapacity2T.VAL_Actl';
-            break;
+            this.areaTagList = 'PEMS_LCHW_ColdCapacity2T.VAL_Actl,PEMS_MCHW_ColdCapacity2T.VAL_Actl,PEMS_RCHW_ColdCapacity2T.VAL_Actl'
+            break
           default:
-            break;
+            break
         }
         this.handlerWaterUseChartSubmit()
       }
@@ -171,17 +188,20 @@ export default {
       this.handlerWaterUseChartSubmit()
     },
     handlerWaterUseChartSubmit() {
-      this.datasource = [
-        { ID: 'TY20210430124568', Name: '贴附薄膜', suplyer: '深圳龙翔材料有限公司', productionDate: '2021-04-30', inWMSDate: '2021-05-01', reciver: '张旭', productionID: 'UYT2021050601', useTime: '2021-05-06 14:30:45'},
-        { ID: 'TY20210430124568', Name: '贴附薄膜', suplyer: '深圳龙翔材料有限公司', productionDate: '2021-04-30', inWMSDate: '2021-05-01', reciver: '张旭', productionID: 'UYT2021050629', useTime: '2021-05-06 14:32:25'},
-        { ID: 'TY20210430124568', Name: '贴附薄膜', suplyer: '深圳龙翔材料有限公司', productionDate: '2021-04-30', inWMSDate: '2021-05-01', reciver: '张旭', productionID: 'UYT2021050678', useTime: '2021-05-06 14:35:23'},
-        { ID: 'TY20210430124568', Name: '贴附薄膜', suplyer: '深圳龙翔材料有限公司', productionDate: '2021-04-30', inWMSDate: '2021-05-01', reciver: '张旭', productionID: 'UYT2021050619', useTime: '2021-05-06 14:36:33'},
-        { ID: 'TY20210430124568', Name: '贴附薄膜', suplyer: '深圳龙翔材料有限公司', productionDate: '2021-04-30', inWMSDate: '2021-05-01', reciver: '张旭', productionID: 'UYT2021050635', useTime: '2021-05-06 14:38:45'},
-        { ID: 'TY20210430124568', Name: '贴附薄膜', suplyer: '深圳龙翔材料有限公司', productionDate: '2021-04-30', inWMSDate: '2021-05-01', reciver: '张旭', productionID: 'UYT2021050625', useTime: '2021-05-06 14:42:56'},
-        { ID: 'TY20210430124568', Name: '贴附薄膜', suplyer: '深圳龙翔材料有限公司', productionDate: '2021-04-30', inWMSDate: '2021-05-01', reciver: '张旭', productionID: 'UYT2021050201', useTime: '2021-05-06 14:45:20'},
-        { ID: 'TY20210430124568', Name: '贴附薄膜', suplyer: '深圳龙翔材料有限公司', productionDate: '2021-04-30', inWMSDate: '2021-05-01', reciver: '张旭', productionID: 'UYT2021050256', useTime: '2021-05-06 14:47:58'}
-      ]
+      let params = this.form
+
+      params['sql_name'] = 'getMaterialUseRecordings'
+      postAction('/common/executeSql', params).then((res) => {
+        this.datasource = res['result']
+      })
     }
+    // handlerRecharge(){
+    //   Vue.set(this.form,"endTime",'');
+    //   Vue.set(this.form,"startTime",'');
+    //   Vue.set(this.form,"productID",'');
+    //   Vue.set(this.form,"materialID",'');
+    //   this.datasource = [];
+    // }
   }
 }
 </script>
@@ -189,6 +209,7 @@ export default {
 <style lang="less" scoped>
 .trend-of-water-use-div {
   margin: -6px -12px 0 0;
+
   /deep/ .ant-card {
     margin-bottom: 8px;
 
