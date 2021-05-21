3fdbe23e598ea25f5e1fb892f238e57b921d86e3
YRK <yrk199512@gmail.com>
2021-05-21 09:19:10 +0800

    删掉websocket调用代码

--- webui/src/components/tools/HeaderNotice.vue
+++ webui/src/components/tools/HeaderNotice.vue
@@ -183,11 +183,11 @@
         var userId = store.getters.userInfo.id;
         var url = window._CONFIG['domianURL'].replace("https://","wss://").replace("http://","ws://")+"/websocket/"+userId;
         //console.log(url);
-        this.websock = new WebSocket(url);
-        this.websock.onopen = this.websocketOnopen;
-        this.websock.onerror = this.websocketOnerror;
-        this.websock.onmessage = this.websocketOnmessage;
-        this.websock.onclose = this.websocketOnclose;
+        // this.websock = new WebSocket(url);
+        // this.websock.onopen = this.websocketOnopen;
+        // this.websock.onerror = this.websocketOnerror;
+        // this.websock.onmessage = this.websocketOnmessage;
+        // this.websock.onclose = this.websocketOnclose;
       },
       websocketOnopen: function () {
         console.log("WebSocket连接成功");
